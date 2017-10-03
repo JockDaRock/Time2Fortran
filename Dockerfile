36c9e2ee6034c9e685c55fdd25cea33eae7c5d3f
JockDaRock <jock.da.rock@gmail.com>
2017-10-02 23:50:27 -0500

    Fortran code execution

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM alpine
+
+RUN apk add -Uuv --no-cache python3 \
+    && apk upgrade -v --available --no-cache \
+    && apk add ca-certificates gfortran libc-dev gcc-multilib \
+    && pip3 install --no-cache-dir --upgrade pip setuptools wheel \
+    && pip3 install requests certifi
+
+ADD https://github.com/alexellis/faas/releases/download/0.5.8-alpha/fwatchdog /usr/bin
+
+RUN chmod +x /usr/bin/fwatchdog
+
+WORKDIR /root/
+
+COPY time2py.py .
+
+ENV fprocess="python3 time2fortran.py"
+
+HEALTHCHECK --interval=1s CMD [ -e /tmp/.lock ] || exit 1
+
+CMD ["fwatchdog"]
---
+++ time2fortran.py
@@ -0,0 +1,43 @@
+import sys
+import os
+from io import StringIO
+import contextlib
+import subprocess
+
+
+@contextlib.contextmanager
+def stdoutIO(stdout=None):
+    old = sys.stdout
+    if stdout is None:
+        stdout = StringIO()
+    sys.stdout = stdout
+    yield stdout
+    sys.stdout = old
+
+
+def get_stdin():
+    buf = ""
+    for line in sys.stdin:
+        buf = buf + line
+    return buf
+
+
+if __name__ == "__main__":
+    st = get_stdin()
+    # print(st)
+    with stdoutIO() as s:
+        try:
+            tmpfile = "/root/main.f95"
+            # print(tmpfile)
+            f = open(tmpfile, 'w')
+            print(st, file=f)
+            f.close()
+            p1 = subprocess.Popen(["gfortran", tmpfile, "-o", "main", "&&", "./main"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p2, p3 = p1.stdout.read().decode('utf-8'), p1.stderr.read().decode('utf-8')
+            if len(p2) > 0:
+                print(p2)
+            else:
+                print(p3)
+        except BaseException as e:
+            print(e)
+    print(s.getvalue())
